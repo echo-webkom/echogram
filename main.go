f19a291c50775f1bde7479b9eeebf2d39057e528
torgerbocianowski <torgerboc@gmail.com>
2024-01-18 12:38:29 +0100

    init azure function

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	images "github.com/echo-webkom/echo-blob/images"
+	jwtware "github.com/gofiber/contrib/jwt"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	// TODO: add auth middleware
+	// jwt := newAuthMiddleware(os.Getenv("SIGNING_KEY"))
+
+	app.Get("/api/images", images.HandleGetImageByFilename)
+	app.Post("/api/images", images.HandlePostImages)
+
+	listenAddr := ":8080"
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		listenAddr = ":" + val
+	}
+
+	log.Fatal(app.Listen(listenAddr))
+}
+
+func newAuthMiddleware(secret string) fiber.Handler {
+	var signKey jwtware.SigningKey
+	signKey.Key = []byte(secret)
+	return jwtware.New(jwtware.Config{
+		SigningKey: signKey,
+	})
+}
