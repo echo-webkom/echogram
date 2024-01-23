c66fe2fe1595533e1ef38533ed132ea1f022fb1d
torgerbocianowski <torgerboc@gmail.com>
2024-01-23 19:37:36 +0100

    update variables

--- deploy-func.sh
+++ deploy-func.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-FUNCTION_APP_NAME=torger-function-app
-RESOURCE_GROUP_NAME=torger-resources
+FUNCTION_APP_NAME=echo-images
+RESOURCE_GROUP_NAME=echo-images-resources
 
 if [ -f handler ]; then
     rm main
